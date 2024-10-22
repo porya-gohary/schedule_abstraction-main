115683e9bd6fdb8d97f31f5f6f967ce20b8fae0f
porya-gohary <gohary.porya@gmail.com>
2024-10-22 16:13:00 +0200

    updating index_set class and version number

--- include/index_set.hpp
+++ include/index_set.hpp
@@ -3,103 +3,131 @@
 
 namespace NP {
 
-		class Index_set
+	class Index_set
+	{
+	public:
+		// Using uint64_t to store 64 bits in each byte
+		typedef std::vector<uint64_t> Set_type;
+
+		// new empty job set
+		Index_set() : the_set() {}
+
+		// derive a new set by "cloning" an existing set and adding an index
+		Index_set(const Index_set& from, std::size_t idx)
+				: the_set(std::max(from.the_set.size(), (idx / 64) + 1))
 		{
-			public:
+			std::copy(from.the_set.begin(), from.the_set.end(), the_set.begin());
+			set_bit(idx, true);
+		}
 
-			typedef std::vector<bool> Set_type;
+		// create the diff of two job sets (intended for debugging only)
+		Index_set(const Index_set &a, const Index_set &b)
+				: the_set(std::max(a.the_set.size(), b.the_set.size()), 0)
+		{
+			auto limit = std::min(a.the_set.size(), b.the_set.size());
+			for (std::size_t i = 0; i < limit; ++i)
+				the_set[i] = a.the_set[i] & ~b.the_set[i];
+		}
 
-			// new empty job set
-			Index_set() : the_set() {}
+		bool operator==(const Index_set &other) const
+		{
+			return the_set == other.the_set;
+		}
 
-			// derive a new set by "cloning" an existing set and adding an index
-			Index_set(const Index_set& from, std::size_t idx)
-			: the_set(std::max(from.the_set.size(), idx + 1))
-			{
-				std::copy(from.the_set.begin(), from.the_set.end(), the_set.begin());
-				the_set[idx] = true;
+		bool operator!=(const Index_set &other) const
+		{
+			return the_set != other.the_set;
+		}
+
+		bool contains(std::size_t idx) const
+		{
+			if (idx / 64 >= the_set.size()) {
+				return false;
+			}
+			return get_bit(idx);
+		}
+
+		bool includes(std::vector<std::size_t> indices) const
+		{
+			for (auto i : indices)
+				if (!contains(i))
+					return false;
+			return true;
+		}
+
+		bool is_subset_of(const Index_set& other) const
+		{
+			// Compare each integer in the sets
+			for (std::size_t i = 0; i < the_set.size(); ++i) {
+				// If there are bits set in the current set that are not set in the other set, it's not a subset
+				if (the_set[i]>0 && (other.the_set.size()<=i || (the_set[i] & other.the_set[i]) != the_set[i])) {
+					return false;
+				}
 			}
 
-			// create the diff of two job sets (intended for debugging only)
-			Index_set(const Index_set &a, const Index_set &b)
-			: the_set(std::max(a.the_set.size(), b.the_set.size()), true)
-			{
-				auto limit = std::min(a.the_set.size(), b.the_set.size());
-				for (unsigned int i = 0; i < limit; i++)
-					the_set[i] = a.contains(i) ^ b.contains(i);
+			return true;
+		}
+
+		std::size_t size() const
+		{
+			std::size_t count = 0;
+			for (std::size_t i = 0; i < the_set.size() * 64; ++i)
+				if (contains(i))
+					count++;
+			return count;
+		}
+
+		void add(std::size_t idx)
+		{
+			if (idx / 64 >= the_set.size())
+				the_set.resize((idx / 64) + 1, 0);
+			set_bit(idx, true);
+		}
+
+		friend std::ostream& operator<< (std::ostream& stream,
+										 const Index_set& s)
+		{
+			bool first = true;
+			stream << "{";
+			for (std::size_t i = 0; i < s.the_set.size() * 64; ++i) {
+				if (s.contains(i)) {
+					if (!first)
+						stream << ", ";
+					first = false;
+					stream << i;
+				}
 			}
+			stream << "}";
 
-			bool operator==(const Index_set &other) const
-			{
-				return the_set == other.the_set;
+			return stream;
+		}
+
+	private:
+
+		Set_type the_set;
+
+		// Helper functions to set and get individual bits
+		void set_bit(std::size_t idx, bool value)
+		{
+			std::size_t byte_index = idx / 64;
+			std::size_t bit_index = idx % 64;
+			if (value) {
+				the_set[byte_index] |= (((uint64_t)1) << bit_index);
+			} else {
+				the_set[byte_index] &= ~(((uint64_t)1) << bit_index);
 			}
+		}
 
-			bool operator!=(const Index_set &other) const
-			{
-				return the_set != other.the_set;
-			}
+		bool get_bit(std::size_t idx) const
+		{
+			std::size_t byte_index = idx / 64;
+			std::size_t bit_index = idx % 64;
+			return the_set[byte_index] & (((uint64_t)1) << bit_index);
+		}
 
-			bool contains(std::size_t idx) const
-			{
-				return the_set.size() > idx && the_set[idx];
-			}
-
-			bool includes(std::vector<std::size_t> indices) const
-			{
-				for (auto i : indices)
-					if (!contains(i))
-						return false;
-				return true;
-			}
-
-			bool is_subset_of(const Index_set& other) const
-			{
-				for (unsigned int i = 0; i < the_set.size(); i++)
-					if (contains(i) && !other.contains(i))
-						return false;
-				return true;
-			}
-
-			std::size_t size() const
-			{
-				std::size_t count = 0;
-				for (auto x : the_set)
-					if (x)
-						count++;
-				return count;
-			}
-
-			void add(std::size_t idx)
-			{
-				if (idx >= the_set.size())
-					the_set.resize(idx + 1);
-				the_set[idx] = true;
-			}
-
-			friend std::ostream& operator<< (std::ostream& stream,
-			                                 const Index_set& s)
-			{
-				bool first = true;
-				stream << "{";
-				for (auto i = 0; i < s.the_set.size(); i++)
-					if (s.the_set[i]) {
-						if (!first)
-							stream << ", ";
-						first = false;
-						stream << i;
-					}
-				stream << "}";
-
-				return stream;
-			}
-
-			private:
-
-			Set_type the_set;
-
-			// no accidental copies
-			Index_set(const Index_set& origin) = delete;
-		};
+		// no accidental copies
+		Index_set(const Index_set& origin) = delete;
+	};
 }
 
 #endif
