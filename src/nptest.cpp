b50708abfff0c0aed5ac3675a64f06cbc5447fa6
Geoffrey Nelissen <g.r.r.j.p.nelissen@tue.nl>
2024-12-05 23:48:47 +0100

    added memory usage monitoring for Windows

--- src/nptest.cpp
+++ src/nptest.cpp
@@ -3,7 +3,11 @@
 #include <fstream>
 #include <algorithm>
 
-#ifndef _WIN32
+#ifdef _WIN32
+#define NOMINMAX
+#include <Windows.h>
+#include <psapi.h>
+#else
 #include <sys/resource.h>
 #endif
 
@@ -252,8 +256,12 @@ static void process_file(const std::string& fname)
 			}
 		}
 
-#ifdef _WIN32 // rusage does not work under Windows
-		long mem_used = 0;
+#ifdef _WIN32 
+		PROCESS_MEMORY_COUNTERS pmc;
+		auto currentProcess = GetCurrentProcess();
+		GetProcessMemoryInfo(currentProcess, &pmc, sizeof(pmc));
+		long mem_used = pmc.PeakWorkingSetSize / 1024; //in kiB
+		CloseHandle(currentProcess);
 #else
 		struct rusage u;
 		long mem_used = 0;
