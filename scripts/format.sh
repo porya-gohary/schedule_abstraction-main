a83b1072b7c50c6e9a56acb2530c8d124fe58564
porya-gohary <gohary.porya@gmail.com>
2025-04-11 14:21:33 +0200

    minor improvement

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# make sure the script is run from the root of the repository
+if [ ! -d ".git" ]; then
+  echo "This script must be run from the root of the repository."
+  exit 1
+fi
+
 FILES=$(find . -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.cc" -o -name "*.c" -o -name "*.h" \) -not -path "./lib/*" -not -path "./build/*")
 echo "+ Running clang-format on the following files:"
 echo $FILES
