802da49e18632fb9614a8579bcc370498a6db407
porya-gohary <gohary.porya@gmail.com>
2025-04-11 13:06:08 +0200

    improvement and exclude lib folder

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-find . -regex '.*\.\(cpp\|hpp\|cc\|c\|h\)' -not -path "./build/*" -exec clang-format -i {} +
+FILES=$(find . -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.cc" -o -name "*.c" -o -name "*.h" \) -not -path "./lib/*" -not -path "./build/*")
+echo "+ Running clang-format on the following files:"
+echo $FILES
+clang-format -i $FILES
